7404e4b33f914620dc0e269fcafb0ea1279926ef
Saoud <saoud@saoud.dev>
2021-05-25 19:15:34 -0700

    adidng client, stylist and HairSalon models

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Salon.Models
+{
+  public class Stylist
+  {
+    public Stylist()
+    {
+      this.Clients = new HashSet<Client>();
+    }
+    public int StylistId { get; set; }
+    public string StylistName { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+  }
+}
